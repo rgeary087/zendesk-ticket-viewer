6fc74e932ec0764ca2dfa684d9ef050cef5b1b41
rgeary087 <54344767+rgeary087@users.noreply.github.com>
2021-11-28 23:55:19 -0500

    added user input

--- curl.bat
+++ curl.bat
@@ -1,3 +1,4 @@
 @echo off
 Rem curl.exe -u %1:%3 https://%2.zendesk.com/api/v2/users/me.json
-curl.exe https://%2.zendesk.com/api/v2/imports/tickets/create_many.json -v -u %1:%3  -X POST -d @tickets.json -H "Content-Type:application/json" 
+Rem curl.exe https://%2.zendesk.com/api/v2/imports/tickets/create_many.json -v -u %1:%3  -X POST -d @tickets.json -H "Content-Type:application/json" 
+curl.exe https://%2.zendesk.com/api/v2/tickets.json?page[size]=25 -v -u %1:%3 -o tickets.json
--- main.py
+++ main.py
@@ -1,15 +1,59 @@
 import subprocess
-import os
 import getpass
+import json
 
-#print("Hello World")
+userEMAIL, userSUBDOMAIN, userPASSWORD = "","",""
+parsed = []
 
-userEMAIL = input('Email:')
-userSUBDOMAIN = input('Subdomain:')
-userPASSWORD = getpass.getpass('Password(hidden):')
+def getUserInfo():
+    global userEMAIL, userSUBDOMAIN, userPASSWORD
+    userEMAIL = input('Email:')
+    userSUBDOMAIN = input('Subdomain:')
+    userPASSWORD = getpass.getpass('Password(hidden):')
 
 #subprocess.call(['bash','curl.sh', userNAME, userPASSWORD])
-data = subprocess.Popen(["curl.bat", userEMAIL, userSUBDOMAIN, userPASSWORD])
+#data = subprocess.Popen(["curl.bat", userEMAIL, userSUBDOMAIN, userPASSWORD])
 
-data.communicate()
+#data.communicate()
+def printTicketList():
+    for ticket in parsed['tickets']:
+        print(f"ID: {ticket['id']} SUB: {ticket['raw_subject']}")
+
+def loadTicketFile():
+    global parsed
+    fileA = open("tickets.json", 'r')
+    parsed = json.load(fileA)
+    printTicketList()
+    fileA.close()
+    
+
+def main():
+    getUserInfo()
+    loadTicketFile()
+    run = True
+
+    while run:
+        print("######################################")
+        choice = input("Enter P to view previous page, N to view next page, an id to view that ticket, or Q to quit: \n")
+        ticketID = -1
+        match choice:
+            case "P":
+                printTicketList()
+                continue
+            case "N":
+                printTicketList()
+                continue
+            case "Q":
+                print("Thank you for using the Ticket Viewer!")
+                quit()
+            case _:
+                try:
+                    ticketID = int(choice)
+                except:
+                    print("Improper variable entered")
+                    continue
+                print(ticketID)    
+
+if(__name__=="__main__"):
+    main()
 
